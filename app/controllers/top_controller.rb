3f727656a30fd306f7fdfba2853cb622c0aa214b
Tomoya Fukuda <2311140073n@ed.fuk.kindai.ac.jp>
2025-10-13 11:16:18 +0000

    発展課題

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,19 +1,25 @@
 class TopController < ApplicationController
-    def main
-        if session[:login_uid] != nil
-            render "main"
-        else
-            render "login"
-            end
-        end
-        
-        def login
-            if params[:uid]== "kindai" and params[:pass] =="sanriko"
-                session[:login_uid]=params[:uid]
-                redirect_to top_main_path
-                
-            else
-                render "error", status: 442
-            end
+  def main
+    if session[:login_uid]
+      render "main"
+    else
+      render "login"
     end
+  end
+
+  def login
+    user = User.find_by(uid: params[:uid])
+
+    if user && BCrypt::Password.new(user.pass) == params[:pass]
+      session[:login_uid] = user.uid
+      redirect_to root_path
+    else
+      render "error", status: 422
+    end
+  end
+
+  def logout
+    session.delete(:login_uid)
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 get 'top/main'
 post 'top/login'
 root 'top#main'
+get 'top/logout', to: 'top#logout'
   
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
